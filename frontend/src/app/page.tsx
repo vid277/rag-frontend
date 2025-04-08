7c076b0da456f40a6508ffccb17cf63c74a05de2
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-04-08 03:11:17 -0400

    refactor: enhance chat page layout and add DotPattern component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,24 +24,33 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen overflow-hidden">
-      <div className="flex flex-col h-full w-1/2 justify-center items-center m-4 gap-8">
-        <PromptSuggestions
-          label="Get started with some questions"
-          append={handleSuggestion}
-          suggestions={[
-            "What is the capital of France?",
-            "What is the capital of Germany?",
-            "What is the capital of Italy?",
-          ]}
-        />
-        <Chat
-          messages={messages as Message[]}
-          input={input}
-          handleInputChange={handleInputChange}
-          handleSubmit={handleSubmit}
-          isGenerating={isLoading}
-          stop={stop}
-        />
+      <div className="flex flex-col h-screen w-1/2 justify-center items-center m-4 gap-8">
+        <div className="flex flex-col gap-4 text-center">
+          <h1 className="text-5xl font-bold">GT RAG Chatbot</h1>
+          <h2 className="text-base text-muted-foreground">
+            learn about Georgia Tech with the help of AI
+          </h2>
+        </div>
+        <div className="flex flex-col gap-2">
+          <PromptSuggestions
+            label="Get started with some questions"
+            append={handleSuggestion}
+            suggestions={[
+              "What is the capital of France?",
+              "What is the capital of Germany?",
+              "What is the capital of Italy?",
+            ]}
+          />
+          <Chat
+            messages={messages as Message[]}
+            input={input}
+            handleInputChange={handleInputChange}
+            handleSubmit={handleSubmit}
+            isGenerating={isLoading}
+            stop={stop}
+            className="overflow-y-auto"
+          />
+        </div>
       </div>
     </div>
   );
